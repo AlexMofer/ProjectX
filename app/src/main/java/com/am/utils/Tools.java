a89a8e768715474620894d06e8118be65399d00f
alex <alex@mazing.com>
2016-01-28 17:44:18 +0800

    判断本地文件是否为图片工具

--- app/src/main/java/com/am/utils/Tools.java
+++ app/src/main/java/com/am/utils/Tools.java
@@ -15,13 +15,31 @@ public class Tools {
      * @return 是否为图片
      */
     public static boolean isBitmapFile(String filePath) {
+        return isBitmapFile(filePath, null);
+    }
+
+    /**
+     * 判断是否为图片
+     *
+     * @param filePath 文件路径
+     * @return 是否为图片
+     */
+    public static boolean isBitmapFile(String filePath, int[] size) {
+        int width;
+        int height;
         try {
             BitmapFactory.Options options = new BitmapFactory.Options();
             options.inJustDecodeBounds = true;
             BitmapFactory.decodeFile(filePath, options);
-            return options.outHeight > 0 && options.outWidth > 0;
+            width = options.outWidth;
+            height = options.outHeight;
         } catch (OutOfMemoryError e) {
             return false;
         }
+        if (size != null && size.length >= 2) {
+            size[0] = width;
+            size[1] = height;
+        }
+        return width > 0 && height > 0;
     }
 }
