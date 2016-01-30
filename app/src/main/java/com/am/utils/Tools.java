035b901641d0badce24a57b721aa41760b1270ef
alex <alex@mazing.com>
2016-01-30 12:01:53 +0800

    工具类更新

--- app/src/main/java/com/am/utils/Tools.java
+++ app/src/main/java/com/am/utils/Tools.java
@@ -1,6 +1,12 @@
 package com.am.utils;
 
+import android.app.ActivityManager;
+import android.content.Context;
 import android.graphics.BitmapFactory;
+
+import com.am.widget.utils.VersionCompat;
+
+import java.util.List;
 
 /**
  * 工具
@@ -41,5 +47,45 @@ public class Tools {
             size[1] = height;
         }
         return width > 0 && height > 0;
+    }
+
+    /**
+     * 获取最大运存
+     * @param context Context
+     * @return 最大运存
+     */
+    public static int getLargeMemoryClass(Context context) {
+        ActivityManager am = (ActivityManager) context
+                .getSystemService(Context.ACTIVITY_SERVICE);
+        int memoryClass = am.getMemoryClass();
+        if (VersionCompat.isLargeHeap(context)) {
+            memoryClass = VersionCompat.getLargeMemoryClass(am);
+        }
+        return memoryClass;
+    }
+
+    /**
+     * 程序是否在前台运行
+     *
+     * @return 是否在前台运行
+     */
+    public static boolean isAppOnForeground(Context context) {
+        // Returns a list of application processes that are running on the device
+        ActivityManager activityManager = (ActivityManager) context
+                .getSystemService(Context.ACTIVITY_SERVICE);
+        String packageName = context.getPackageName();
+        List<ActivityManager.RunningAppProcessInfo> appProcesses = activityManager
+                .getRunningAppProcesses();
+        if (appProcesses == null)
+            return false;
+        for (ActivityManager.RunningAppProcessInfo appProcess : appProcesses) {
+            // The name of the process that this object is associated with.
+            if (appProcess.processName.equals(packageName)
+                    && appProcess.importance == ActivityManager
+                    .RunningAppProcessInfo.IMPORTANCE_FOREGROUND) {
+                return true;
+            }
+        }
+        return false;
     }
 }
