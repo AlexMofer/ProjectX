d1e0f7b1133980436ca8ae5f899d393d605a28ee
Alex <alex@mazing.com>
2016-09-19 11:49:29 +0800

    Blog 更新

--- app/src/main/java/am/project/x/activities/develop/test/TestActivity.java
+++ app/src/main/java/am/project/x/activities/develop/test/TestActivity.java
@@ -2,7 +2,10 @@ package am.project.x.activities.develop.test;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import am.project.x.R;
 import am.project.x.activities.BaseActivity;
@@ -17,6 +20,25 @@ public class TestActivity extends BaseActivity {
     @Override
     protected void initResource(Bundle savedInstanceState) {
         setSupportActionBar(R.id.test_toolbar);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(getApplicationContext(), Boolean.toString(checkApkExist(getApplicationContext(), "com.tencent.mm")), Toast.LENGTH_SHORT).show();
+    }
+
+    @SuppressWarnings("all")
+    public boolean checkApkExist(Context context, String packageName) {
+        try {
+            ApplicationInfo info = context.getPackageManager().getApplicationInfo(packageName,
+                    PackageManager.GET_UNINSTALLED_PACKAGES);
+            if (info != null)
+                return true;
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
+        return false;
     }
 
     public static void startActivity(Context context) {
--- blogs/README.md
+++ blogs/README.md
@@ -3,3 +3,4 @@
 
 ##Skill
 - [***慎用onBackPressed()***](https://github.com/AlexMofer/ProjectX/blob/master/blogs/%5BSkill%5D%E6%85%8E%E7%94%A8onBackPressed().md)
+- [***getApplicationInfo的坑***](https://github.com/AlexMofer/ProjectX/blob/master/blogs/%5BSkill%5DgetApplicationInfo%E7%9A%84%E5%9D%91.md)
