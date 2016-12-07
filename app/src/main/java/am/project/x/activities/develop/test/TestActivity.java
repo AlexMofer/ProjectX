73cca5f71758b3cdfcc8e35a126b5167999433d1
Alex <alex@mazing.com>
2016-12-07 19:12:11 +0800

    拖动视图

--- app/src/main/java/am/project/x/activities/develop/test/TestActivity.java
+++ app/src/main/java/am/project/x/activities/develop/test/TestActivity.java
@@ -3,6 +3,8 @@ package am.project.x.activities.develop.test;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import am.project.x.R;
 import am.project.x.activities.BaseActivity;
@@ -18,6 +20,11 @@ public class TestActivity extends BaseActivity {
     @Override
     protected void initResource(Bundle savedInstanceState) {
         setSupportActionBar(R.id.test_toolbar);
+    }
+
+    public void show(View view) {
+        Toast.makeText(this, "点击", Toast.LENGTH_SHORT).show();
+        findViewById(R.id.temp).setVisibility(View.VISIBLE);
     }
 
     public static void startActivity(Context context) {
