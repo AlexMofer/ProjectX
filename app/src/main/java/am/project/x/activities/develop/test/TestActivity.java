efce6d86b8e2d743b4ae1800cb83c4945681eddf
Alex <moferalex@gmail.com>
2017-03-19 15:22:26 +0800

    去除失效的控件

--- app/src/main/java/am/project/x/activities/develop/test/TestActivity.java
+++ app/src/main/java/am/project/x/activities/develop/test/TestActivity.java
@@ -3,15 +3,9 @@ package am.project.x.activities.develop.test;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MotionEvent;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Toast;
 
 import am.project.x.R;
 import am.project.x.activities.BaseActivity;
-import am.widget.draglayout.DragLayout;
-import am.widget.treelayout.TreeLayout;
 
 public class TestActivity extends BaseActivity {
 
@@ -24,31 +18,6 @@ public class TestActivity extends BaseActivity {
     @Override
     protected void initResource(Bundle savedInstanceState) {
         setSupportActionBar(R.id.test_toolbar);
-        findViewById(R.id.drag).setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                TreeLayout treeLayout = (TreeLayout) ((ViewGroup) view).getChildAt(0);
-                if (treeLayout.isExpand()) {
-                    treeLayout.setExpand(false);
-                    DragLayout.LayoutParams lp = (DragLayout.LayoutParams) treeLayout.getLayoutParams();
-                    lp.setDraggable(true);
-                    return true;
-                }
-                return false;
-            }
-        });
-    }
-
-    public void show(View view) {
-        TreeLayout treeLayout = (TreeLayout) view.getParent();
-        DragLayout.LayoutParams lp = (DragLayout.LayoutParams) treeLayout.getLayoutParams();
-        treeLayout.setRight(!lp.isCloseToStart());
-        treeLayout.setExpand(!treeLayout.isExpand());
-        lp.setDraggable(!lp.isDraggable());
-    }
-
-    public void message(View view) {
-        Toast.makeText(this, "message", Toast.LENGTH_SHORT).show();
     }
 
     public static void startActivity(Context context) {
