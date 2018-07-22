bc2dd61c7ceb10c5bd8ccae1922daa5209b6e795
Alex <moferalex@gmail.com>
2018-07-22 23:38:29 +0800

    [update] 开始项目重构。

--- app/src/main/java/am/project/x/widgets/supergridview/animator/AnimatorCallback.java
+++
@@ -1,9 +0,0 @@
-package am.project.x.widgets.supergridview.animator;
-
-public interface AnimatorCallback {
-	void isStart();
-
-	void isStop();
-
-	void isEnd();
-}
--- shapeimageview/history/README.md
+++
@@ -1,2 +0,0 @@
-# 历史
-- [**1.1.0**](https://github.com/AlexMofer/ProjectX/tree/master/shapeimageview/history/1.1.0)
