f67e25fca9312a2fa35859e92a494b438ccd4bde
Alex <moferalex@gmail.com>
2018-08-01 23:59:31 +0800

    [update] 增加游标顶部栏页面

---
+++ app/src/main/java/am/project/x/business/widgets/indicatortabstrip/IndicatorTabStripActivity.java
@@ -0,0 +1,20 @@
+package am.project.x.business.widgets.indicatortabstrip;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import am.project.x.R;
+import am.project.x.base.BaseActivity;
+
+public class IndicatorTabStripActivity extends BaseActivity {
+
+    @Override
+    protected int getContentViewLayout() {
+        return R.layout.activity_indicatortabstrip;
+    }
+
+    @Override
+    protected void initializeActivity(@Nullable Bundle savedInstanceState) {
+
+    }
+}
