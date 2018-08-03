a16ccb0108900c12fb01197ff24e465cd783e3e2
Alex <moferalex@gmail.com>
2018-08-04 03:21:16 +0800

    [update] 完成状态布局页面点击事件

--- app/src/main/java/am/project/x/business/widgets/statelayout/StateLayoutActivity.java
+++ app/src/main/java/am/project/x/business/widgets/statelayout/StateLayoutActivity.java
@@ -17,16 +17,31 @@ package am.project.x.business.widgets.statelayout;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.AppCompatTextView;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.RadioGroup;
 
 import am.project.x.R;
 import am.project.x.base.BaseActivity;
+import am.widget.stateframelayout.StateFrameLayout;
 
 /**
  * 状态布局
  */
-public class StateLayoutActivity extends BaseActivity {
+public class StateLayoutActivity extends BaseActivity implements
+        RadioGroup.OnCheckedChangeListener {
+
+    private StateFrameLayout mVState;
+    private Drawable mDLoading;
+    private Drawable mDError;
+    private Drawable mDEmpty;
+    private View mVLoading;
+    private View mVError;
+    private View mVEmpty;
 
     public static void start(Context context) {
         context.startActivity(new Intent(context, StateLayoutActivity.class));
@@ -40,5 +55,54 @@ public class StateLayoutActivity extends BaseActivity {
     @Override
     protected void initializeActivity(@Nullable Bundle savedInstanceState) {
         setSupportActionBar(R.id.sl_toolbar);
+        mVState = findViewById(R.id.sl_lyt_state);
+        final RadioGroup state = findViewById(R.id.sl_rg_state);
+        final RadioGroup mode = findViewById(R.id.sl_rg_mode);
+
+        final AppCompatTextView loading = new AppCompatTextView(this);
+        loading.setText(R.string.sl_change_state_loading);
+        loading.setTextColor(0xfff2f71c);
+        loading.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 64);
+        mVLoading = loading;
+        final AppCompatTextView error = new AppCompatTextView(this);
+        error.setText(R.string.sl_change_state_error);
+        error.setTextColor(0xffff4081);
+        error.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 64);
+        mVError = error;
+        final AppCompatTextView empty = new AppCompatTextView(this);
+        empty.setText(R.string.sl_change_state_empty);
+        empty.setTextColor(0xff092d6d);
+        empty.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 64);
+        mVEmpty = empty;
+
+
+        state.setOnCheckedChangeListener(this);
+        state.check(R.id.sl_rb_normal);
+        mode.setOnCheckedChangeListener(this);
+        mode.check(R.id.sl_rb_drawable);
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        switch (checkedId) {
+            case R.id.sl_rb_normal:
+                mVState.normal();
+                break;
+            case R.id.sl_rb_loading:
+                mVState.loading();
+                break;
+            case R.id.sl_rb_error:
+                mVState.error();
+                break;
+            case R.id.sl_rb_empty:
+                mVState.empty();
+                break;
+            case R.id.sl_rb_drawable:
+//                mVState.setStateDrawables(mLoadingDrawable, mErrorDrawable, mEmptyDrawable);
+                break;
+            case R.id.sl_rb_view:
+                mVState.setStateViews(mVLoading, mVError, mVEmpty);
+                break;
+        }
     }
 }
