48c261e3f40755cc107e0c45872fbc44d3dca23d
Alex <moferalex@gmail.com>
2018-08-06 00:40:30 +0800

    [update] 完成WrapLayout页面逻辑

--- app/src/main/java/am/project/x/business/widgets/wraplayout/WrapLayoutActivity.java
+++ app/src/main/java/am/project/x/business/widgets/wraplayout/WrapLayoutActivity.java
@@ -19,14 +19,20 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
 
 import am.project.x.R;
 import am.project.x.base.BaseActivity;
+import am.widget.wraplayout.WrapLayout;
 
 /**
  * 自动换行布局
  */
-public class WrapLayoutActivity extends BaseActivity {
+public class WrapLayoutActivity extends BaseActivity implements RadioGroup.OnCheckedChangeListener,
+        SeekBar.OnSeekBarChangeListener {
+
+    private WrapLayout mVContent;
 
     public static void start(Context context) {
         context.startActivity(new Intent(context, WrapLayoutActivity.class));
@@ -40,5 +46,56 @@ public class WrapLayoutActivity extends BaseActivity {
     @Override
     protected void initializeActivity(@Nullable Bundle savedInstanceState) {
         setSupportActionBar(R.id.wl_toolbar);
+        mVContent = findViewById(R.id.wl_wl_content);
+        final RadioGroup gravity = findViewById(R.id.wl_rg_gravity);
+        final SeekBar horizontal = findViewById(R.id.wl_sb_horizontal);
+        final SeekBar vertical = findViewById(R.id.wl_sb_vertical);
+
+        gravity.setOnCheckedChangeListener(this);
+        gravity.check(R.id.wl_rb_top);
+        horizontal.setOnSeekBarChangeListener(this);
+        horizontal.setProgress(15);
+        vertical.setOnSeekBarChangeListener(this);
+        vertical.setProgress(15);
+    }
+
+    // Listener
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        switch (checkedId) {
+            case R.id.wl_rb_top:
+                mVContent.setGravity(WrapLayout.GRAVITY_TOP);
+                break;
+            case R.id.wl_rb_center:
+                mVContent.setGravity(WrapLayout.GRAVITY_CENTER);
+                break;
+            case R.id.wl_rb_bottom:
+                mVContent.setGravity(WrapLayout.GRAVITY_BOTTOM);
+                break;
+        }
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        switch (seekBar.getId()) {
+            case R.id.wl_sb_horizontal:
+                mVContent.setHorizontalSpacing(
+                        (int) (progress * getResources().getDisplayMetrics().density));
+                break;
+            case R.id.wl_sb_vertical:
+                mVContent.setVerticalSpacing(
+                        (int) (progress * getResources().getDisplayMetrics().density));
+                break;
+        }
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
     }
 }
