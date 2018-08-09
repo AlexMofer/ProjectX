2e3ec56fcec7a58dd1b2c8c2323e29c8ef1f015d
Alex <moferalex@gmail.com>
2018-08-10 02:02:56 +0800

    [update] 完成CircleProgressBar基础属性控制逻辑

--- app/src/main/java/am/project/x/business/widgets/circleprogressbar/CircleProgressBarActivity.java
+++ app/src/main/java/am/project/x/business/widgets/circleprogressbar/CircleProgressBarActivity.java
@@ -19,14 +19,27 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.CompoundButton;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.Switch;
 
 import am.project.x.R;
 import am.project.x.base.BaseActivity;
+import am.widget.circleprogressbar.CircleProgressBar;
 
 /**
  * 环形进度条
  */
-public class CircleProgressBarActivity extends BaseActivity {
+public class CircleProgressBarActivity extends BaseActivity implements
+        AdapterView.OnItemSelectedListener, SeekBar.OnSeekBarChangeListener,
+        CompoundButton.OnCheckedChangeListener {
+
+    private CircleProgressBar mProgress;
+    private float density;
 
     public static void start(Context context) {
         context.startActivity(new Intent(context, CircleProgressBarActivity.class));
@@ -40,5 +53,108 @@ public class CircleProgressBarActivity extends BaseActivity {
     @Override
     protected void initializeActivity(@Nullable Bundle savedInstanceState) {
         setSupportActionBar(R.id.cpb_toolbar);
+        mProgress = findViewById(R.id.cpb_cpb_bar);
+        density = getResources().getDisplayMetrics().density;
+
+        this.<Spinner>findViewById(R.id.cpb_sp_gravity).setOnItemSelectedListener(this);
+        this.<SeekBar>findViewById(R.id.cpb_sb_radius).setOnSeekBarChangeListener(this);
+        this.<Spinner>findViewById(R.id.cpb_sp_scale_type).setOnItemSelectedListener(this);
+
+        this.<Switch>findViewById(R.id.cpb_sh_progress_mode).setOnCheckedChangeListener(this);
+
+    }
+
+    // Listener
+    @SuppressWarnings("all")
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        switch (parent.getId()) {
+            case R.id.cpb_sp_gravity:
+                switch (position) {
+                    default:
+                    case 0:
+                        mProgress.setGravity(Gravity.CENTER);
+                        break;
+                    case 1:
+                        mProgress.setGravity(Gravity.LEFT);
+                        break;
+                    case 2:
+                        mProgress.setGravity(Gravity.CENTER_HORIZONTAL);
+                        break;
+                    case 3:
+                        mProgress.setGravity(Gravity.RIGHT);
+                        break;
+                    case 4:
+                        mProgress.setGravity(Gravity.CENTER_VERTICAL);
+                        break;
+                    case 5:
+                        mProgress.setGravity(Gravity.CENTER_VERTICAL | Gravity.RIGHT);
+                        break;
+                    case 6:
+                        mProgress.setGravity(Gravity.BOTTOM);
+                        break;
+                    case 7:
+                        mProgress.setGravity(Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL);
+                        break;
+                    case 8:
+                        mProgress.setGravity(Gravity.BOTTOM | Gravity.RIGHT);
+                        break;
+                }
+                break;
+            case R.id.cpb_sp_scale_type:
+                switch (position) {
+                    default:
+                    case 0:
+                        mProgress.setScaleType(CircleProgressBar.ST_INSIDE);
+                        break;
+                    case 1:
+                        mProgress.setScaleType(CircleProgressBar.ST_CROP);
+                        break;
+                    case 2:
+                        mProgress.setScaleType(
+                                CircleProgressBar.ST_INSIDE | CircleProgressBar.ST_CROP);
+                        break;
+                    case 3:
+                        mProgress.setScaleType(CircleProgressBar.ST_NONE);
+                        break;
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        // do nothing
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        switch (seekBar.getId()) {
+            case R.id.cpb_sb_radius:
+                mProgress.setRadius(density * (progress + 100));
+                break;
+
+        }
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+        // do nothing
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        // do nothing
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        switch (buttonView.getId()) {
+            case R.id.cpb_sh_progress_mode:
+                mProgress.setProgressMode(isChecked ?
+                        CircleProgressBar.ProgressMode.LOADING :
+                        CircleProgressBar.ProgressMode.PROGRESS);
+                break;
+        }
     }
 }
