c7bf34b863d87ffcf69e26b204800fb0f167f07e
Alex <moferalex@gmail.com>
2018-08-13 00:46:12 +0800

    [update] 完成CircleProgressBar页面逻辑

--- app/src/main/java/am/project/x/business/widgets/circleprogressbar/CircleProgressBarActivity.java
+++ app/src/main/java/am/project/x/business/widgets/circleprogressbar/CircleProgressBarActivity.java
@@ -15,6 +15,7 @@
  */
 package am.project.x.business.widgets.circleprogressbar;
 
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -36,7 +37,7 @@ import am.widget.circleprogressbar.CircleProgressBar;
  */
 public class CircleProgressBarActivity extends BaseActivity implements
         AdapterView.OnItemSelectedListener, SeekBar.OnSeekBarChangeListener,
-        CompoundButton.OnCheckedChangeListener {
+        CompoundButton.OnCheckedChangeListener, View.OnClickListener {
 
     private CircleProgressBar mProgress;
     private float density;
@@ -61,7 +62,45 @@ public class CircleProgressBarActivity extends BaseActivity implements
         this.<Spinner>findViewById(R.id.cpb_sp_scale_type).setOnItemSelectedListener(this);
 
         this.<Switch>findViewById(R.id.cpb_sh_progress_mode).setOnCheckedChangeListener(this);
+        this.<SeekBar>findViewById(R.id.cpb_sb_loading_start_angle)
+                .setOnSeekBarChangeListener(this);
+        this.<SeekBar>findViewById(R.id.cpb_sb_loading_sweep_angle)
+                .setOnSeekBarChangeListener(this);
+        this.<SeekBar>findViewById(R.id.cpb_sb_loading_duration)
+                .setOnSeekBarChangeListener(this);
+        this.<Switch>findViewById(R.id.cpb_sh_loading_repeat_mode).setOnCheckedChangeListener(this);
+        this.<Switch>findViewById(R.id.cpb_sh_loading_draw_other).setOnCheckedChangeListener(this);
 
+        this.<SeekBar>findViewById(R.id.cpb_sb_start_angle).setOnSeekBarChangeListener(this);
+        this.<SeekBar>findViewById(R.id.cpb_sb_sweep_angle).setOnSeekBarChangeListener(this);
+        this.<SeekBar>findViewById(R.id.cpb_sb_background_size).setOnSeekBarChangeListener(this);
+        this.<SeekBar>findViewById(R.id.cpb_sb_progress_size).setOnSeekBarChangeListener(this);
+        this.<SeekBar>findViewById(R.id.cpb_sb_progress).setOnSeekBarChangeListener(this);
+        this.<Switch>findViewById(R.id.cpb_sh_gradient).setOnCheckedChangeListener(this);
+        this.<Switch>findViewById(R.id.cpb_sh_progress_value).setOnCheckedChangeListener(this);
+        this.<SeekBar>findViewById(R.id.cpb_sb_progress_value_text_size)
+                .setOnSeekBarChangeListener(this);
+
+        this.<SeekBar>findViewById(R.id.cpb_sb_top_text_gap).setOnSeekBarChangeListener(this);
+        this.<SeekBar>findViewById(R.id.cpb_sb_top_text_size).setOnSeekBarChangeListener(this);
+        this.<SeekBar>findViewById(R.id.cpb_sb_bottom_text_gap).setOnSeekBarChangeListener(this);
+        this.<SeekBar>findViewById(R.id.cpb_sb_bottom_text_size).setOnSeekBarChangeListener(this);
+        this.<SeekBar>findViewById(R.id.cpb_sb_progress_duration).setOnSeekBarChangeListener(this);
+
+        this.<Spinner>findViewById(R.id.cpb_sp_dial_visibility).setOnItemSelectedListener(this);
+        this.<SeekBar>findViewById(R.id.cpb_sb_dial_gap).setOnSeekBarChangeListener(this);
+        this.<SeekBar>findViewById(R.id.cpb_sb_dial_angle).setOnSeekBarChangeListener(this);
+        this.<SeekBar>findViewById(R.id.cpb_sb_dial_height).setOnSeekBarChangeListener(this);
+        this.<SeekBar>findViewById(R.id.cpb_sb_dial_width).setOnSeekBarChangeListener(this);
+        this.<SeekBar>findViewById(R.id.cpb_sb_dial_special_unit).setOnSeekBarChangeListener(this);
+        this.<SeekBar>findViewById(R.id.cpb_sb_dial_special_height).setOnSeekBarChangeListener(this);
+        this.<SeekBar>findViewById(R.id.cpb_sb_dial_special_width).setOnSeekBarChangeListener(this);
+        this.<Spinner>findViewById(R.id.cpb_sp_dial_gravity).setOnItemSelectedListener(this);
+        this.<Switch>findViewById(R.id.cpb_sh_special_dial_value).setOnCheckedChangeListener(this);
+        this.<SeekBar>findViewById(R.id.cpb_sb_special_dial_value_gap).setOnSeekBarChangeListener(this);
+        this.<SeekBar>findViewById(R.id.cpb_sb_special_dial_value_text_size)
+                .setOnSeekBarChangeListener(this);
+        findViewById(R.id.cpb_btn_progress_animator).setOnClickListener(this);
     }
 
     // Listener
@@ -119,6 +158,34 @@ public class CircleProgressBarActivity extends BaseActivity implements
                         break;
                 }
                 break;
+            case R.id.cpb_sp_dial_visibility:
+                switch (position) {
+                    default:
+                    case 0:
+                        mProgress.setDialVisibility(View.VISIBLE);
+                        break;
+                    case 1:
+                        mProgress.setDialVisibility(View.INVISIBLE);
+                        break;
+                    case 2:
+                        mProgress.setDialVisibility(View.GONE);
+                        break;
+                }
+                break;
+            case R.id.cpb_sp_dial_gravity:
+                switch (position) {
+                    default:
+                    case 0:
+                        mProgress.setDialGravity(Gravity.CENTER);
+                        break;
+                    case 1:
+                        mProgress.setDialGravity(Gravity.TOP);
+                        break;
+                    case 2:
+                        mProgress.setDialGravity(Gravity.BOTTOM);
+                        break;
+                }
+                break;
         }
     }
 
@@ -133,7 +200,75 @@ public class CircleProgressBarActivity extends BaseActivity implements
             case R.id.cpb_sb_radius:
                 mProgress.setRadius(density * (progress + 100));
                 break;
-
+            case R.id.cpb_sb_loading_start_angle:
+                mProgress.setLoadingStartAngle(progress);
+                break;
+            case R.id.cpb_sb_loading_sweep_angle:
+                mProgress.setLoadingSweepAngle(progress);
+                break;
+            case R.id.cpb_sb_loading_duration:
+                mProgress.setLoadingDuration(100 * (progress + 1));
+                break;
+            case R.id.cpb_sb_start_angle:
+                mProgress.setStartAngle(progress);
+                break;
+            case R.id.cpb_sb_sweep_angle:
+                mProgress.setSweepAngle(progress);
+                break;
+            case R.id.cpb_sb_background_size:
+                mProgress.setBackgroundSize(density * progress);
+                break;
+            case R.id.cpb_sb_progress_size:
+                mProgress.setProgressSize(density * progress);
+                break;
+            case R.id.cpb_sb_progress:
+                mProgress.animationToProgress(progress);
+                break;
+            case R.id.cpb_sb_progress_value_text_size:
+                mProgress.setProgressValueTextSize(density * (progress + 50));
+                break;
+            case R.id.cpb_sb_top_text_gap:
+                mProgress.setTopTextGap(density * progress);
+                break;
+            case R.id.cpb_sb_top_text_size:
+                mProgress.setTopTextSize(density * (progress + 10));
+                break;
+            case R.id.cpb_sb_bottom_text_gap:
+                mProgress.setBottomTextGap(density * progress);
+                break;
+            case R.id.cpb_sb_bottom_text_size:
+                mProgress.setBottomTextSize(density * (progress + 10));
+                break;
+            case R.id.cpb_sb_progress_duration:
+                mProgress.setProgressDuration(100 * (progress + 1));
+                break;
+            case R.id.cpb_sb_dial_gap:
+                mProgress.setDialGap(density * progress);
+                break;
+            case R.id.cpb_sb_dial_angle:
+                mProgress.setDialAngle(progress);
+                break;
+            case R.id.cpb_sb_dial_height:
+                mProgress.setDialHeight(density * progress);
+                break;
+            case R.id.cpb_sb_dial_width:
+                mProgress.setDialWidth(density * progress);
+                break;
+            case R.id.cpb_sb_dial_special_unit:
+                mProgress.setDialSpecialUnit(progress);
+                break;
+            case R.id.cpb_sb_dial_special_height:
+                mProgress.setDialSpecialHeight(density * progress);
+                break;
+            case R.id.cpb_sb_dial_special_width:
+                mProgress.setDialSpecialWidth(density * progress);
+                break;
+            case R.id.cpb_sb_special_dial_value_gap:
+                mProgress.setSpecialDialValueGap(density * progress);
+                break;
+            case R.id.cpb_sb_special_dial_value_text_size:
+                mProgress.setSpecialDialValueTextSize(density * (progress + 5));
+                break;
         }
     }
 
@@ -154,6 +289,35 @@ public class CircleProgressBarActivity extends BaseActivity implements
                 mProgress.setProgressMode(isChecked ?
                         CircleProgressBar.ProgressMode.LOADING :
                         CircleProgressBar.ProgressMode.PROGRESS);
+                break;
+            case R.id.cpb_sh_loading_repeat_mode:
+                mProgress.setLoadingRepeatMode(isChecked ? ValueAnimator.REVERSE :
+                        ValueAnimator.RESTART);
+                break;
+            case R.id.cpb_sh_loading_draw_other:
+                mProgress.setLoadingDrawOther(isChecked);
+                break;
+            case R.id.cpb_sh_gradient:
+                if (isChecked)
+                    mProgress.setGradientColors(0xff33b5e5, 0xff99cc00, 0xffffbb33,
+                            0xffff4444, 0xff33b5e5);
+                else
+                    mProgress.setGradientColors(0xffff4444);
+                break;
+            case R.id.cpb_sh_progress_value:
+                mProgress.setShowSpecialDialValue(isChecked);
+                break;
+            case R.id.cpb_sh_special_dial_value:
+                mProgress.setShowSpecialDialValue(isChecked);
+                break;
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.cpb_btn_progress_animator:
+                mProgress.animationToProgress(0, mProgress.getProgress());
                 break;
         }
     }
