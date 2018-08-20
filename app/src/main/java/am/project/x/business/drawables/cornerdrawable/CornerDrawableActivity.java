6885469145a11011dcbad33321ec175a2d5dd925
Alex <moferalex@gmail.com>
2018-08-21 01:44:24 +0800

    [update] 完成尖角框页逻辑

--- app/src/main/java/am/project/x/business/drawables/cornerdrawable/CornerDrawableActivity.java
+++ app/src/main/java/am/project/x/business/drawables/cornerdrawable/CornerDrawableActivity.java
@@ -17,16 +17,29 @@ package am.project.x.business.drawables.cornerdrawable;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.SeekBar;
+import android.widget.Spinner;
 
+import am.drawable.CornerDrawable;
 import am.project.x.R;
 import am.project.x.base.BaseActivity;
 
 /**
  * 尖角框
  */
-public class CornerDrawableActivity extends BaseActivity {
+public class CornerDrawableActivity extends BaseActivity implements
+        AdapterView.OnItemSelectedListener, SeekBar.OnSeekBarChangeListener {
+
+    private CornerDrawable drawable;
+    private int stokeColor;
+    private float density;
 
     public static void start(Context context) {
         context.startActivity(new Intent(context, CornerDrawableActivity.class));
@@ -40,5 +53,112 @@ public class CornerDrawableActivity extends BaseActivity {
     @Override
     protected void initializeActivity(@Nullable Bundle savedInstanceState) {
         setSupportActionBar(R.id.cnd_toolbar);
+        density = getResources().getDisplayMetrics().density;
+        stokeColor = ContextCompat.getColor(this, R.color.colorAccent);
+        final int color = ContextCompat.getColor(this, R.color.colorRipple);
+        final int width = (int) (20 * density);
+        final int height = (int) (10 * density);
+        drawable = new CornerDrawable(color, width, height);
+        if (Build.VERSION.SDK_INT >= 16) {
+            findViewById(R.id.cnd_tv_content).setBackground(drawable);
+        } else {
+            findViewById(R.id.cnd_tv_content).setBackgroundDrawable(drawable);
+        }
+        this.<Spinner>findViewById(R.id.cnd_sp_direction).setOnItemSelectedListener(this);
+        this.<Spinner>findViewById(R.id.cnd_sp_location).setOnItemSelectedListener(this);
+        this.<SeekBar>findViewById(R.id.cnd_sb_width).setOnSeekBarChangeListener(this);
+        this.<SeekBar>findViewById(R.id.cnd_sb_height).setOnSeekBarChangeListener(this);
+        this.<SeekBar>findViewById(R.id.cnd_sb_margin).setOnSeekBarChangeListener(this);
+        this.<SeekBar>findViewById(R.id.cnd_sb_bezier).setOnSeekBarChangeListener(this);
+        this.<SeekBar>findViewById(R.id.cnd_sb_stoke).setOnSeekBarChangeListener(this);
+        this.<SeekBar>findViewById(R.id.cnd_sb_radius).setOnSeekBarChangeListener(this);
+        this.<SeekBar>findViewById(R.id.cnd_sb_padding).setOnSeekBarChangeListener(this);
+    }
+
+    // Listener
+    @Override
+    @SuppressWarnings("all")
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        switch (parent.getId()) {
+            case R.id.cnd_sp_direction:
+                switch (position) {
+                    default:
+                    case 0:
+                        drawable.setDirection(Gravity.TOP);
+                        break;
+                    case 1:
+                        drawable.setDirection(Gravity.LEFT);
+                        break;
+                    case 2:
+                        drawable.setDirection(Gravity.RIGHT);
+                        break;
+                    case 3:
+                        drawable.setDirection(Gravity.BOTTOM);
+                        break;
+                }
+                break;
+            case R.id.cnd_sp_location:
+                switch (position) {
+                    default:
+                    case 0:
+                        drawable.setLocation(Gravity.CENTER);
+                        break;
+                    case 1:
+                        drawable.setLocation(Gravity.LEFT);
+                        break;
+                    case 2:
+                        drawable.setLocation(Gravity.RIGHT);
+                        break;
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        switch (seekBar.getId()) {
+            case R.id.cnd_sb_width:
+                final int width = (int) ((20 + progress) * density);
+                drawable.setCornerWidth(width);
+                break;
+            case R.id.cnd_sb_height:
+                final int height = (int) ((10 + progress) * density);
+                drawable.setCornerHeight(height);
+                break;
+            case R.id.cnd_sb_margin:
+                final int margin = (int) (progress * density);
+                drawable.setCornerMargin(margin);
+                break;
+            case R.id.cnd_sb_bezier:
+                final float bezier = progress * 0.01f;
+                drawable.setCornerBezier(bezier);
+                break;
+            case R.id.cnd_sb_stoke:
+                drawable.setStroke(progress, stokeColor, 0, 0);
+                break;
+            case R.id.cnd_sb_radius:
+                final int radius = (int) (progress * density);
+                drawable.setContentRadius(radius);
+                break;
+            case R.id.cnd_sb_padding:
+                final int padding = (int) (progress * density);
+                drawable.setPadding(padding, padding, padding, padding);
+                break;
+        }
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
     }
 }
