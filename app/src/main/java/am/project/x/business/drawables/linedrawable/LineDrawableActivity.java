a49833f0354d1b82de68d5f7755cc4fa7023a103
Alex <moferalex@gmail.com>
2018-08-22 00:59:07 +0800

    [update] 完成横线图片页面逻辑

--- app/src/main/java/am/project/x/business/drawables/linedrawable/LineDrawableActivity.java
+++ app/src/main/java/am/project/x/business/drawables/linedrawable/LineDrawableActivity.java
@@ -17,16 +17,27 @@ package am.project.x.business.drawables.linedrawable;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.SeekBar;
+import android.widget.Spinner;
 
+import am.drawable.LineDrawable;
 import am.project.x.R;
 import am.project.x.base.BaseActivity;
 
 /**
  * 横线图片
  */
-public class LineDrawableActivity extends BaseActivity {
+public class LineDrawableActivity extends BaseActivity implements
+        AdapterView.OnItemSelectedListener, SeekBar.OnSeekBarChangeListener {
+
+    private LineDrawable drawable;
 
     public static void start(Context context) {
         context.startActivity(new Intent(context, LineDrawableActivity.class));
@@ -40,5 +51,67 @@ public class LineDrawableActivity extends BaseActivity {
     @Override
     protected void initializeActivity(@Nullable Bundle savedInstanceState) {
         setSupportActionBar(R.id.lid_toolbar);
+        drawable = new LineDrawable(ContextCompat.getColor(this, R.color.colorPrimary),
+                1);
+        if (Build.VERSION.SDK_INT >= 16) {
+            findViewById(R.id.lid_tv_content).setBackground(drawable);
+        } else {
+            findViewById(R.id.lid_tv_content).setBackgroundDrawable(drawable);
+        }
+        this.<Spinner>findViewById(R.id.lid_sp_gravity).setOnItemSelectedListener(this);
+        this.<SeekBar>findViewById(R.id.lid_sb_size).setOnSeekBarChangeListener(this);
+    }
+
+    // Listener
+    @SuppressWarnings("all")
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        switch (position) {
+            default:
+            case 0:
+                drawable.setGravity(Gravity.BOTTOM);
+                break;
+            case 1:
+                drawable.setGravity(Gravity.LEFT);
+                break;
+            case 2:
+                drawable.setGravity(Gravity.TOP);
+                break;
+            case 3:
+                drawable.setGravity(Gravity.RIGHT);
+                break;
+            case 4:
+                drawable.setGravity(Gravity.CENTER_HORIZONTAL);
+                break;
+            case 5:
+                drawable.setGravity(Gravity.CENTER_VERTICAL);
+                break;
+            case 6:
+                drawable.setGravity(Gravity.TOP | Gravity.BOTTOM);
+                break;
+            case 7:
+                drawable.setGravity(Gravity.LEFT | Gravity.RIGHT);
+                break;
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        drawable.setLineSize(progress + 1);
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
     }
 }
