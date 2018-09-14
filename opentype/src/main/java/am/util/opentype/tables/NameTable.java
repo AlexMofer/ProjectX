1d44e2b03fa1b7542d1ba585e9d1493917d4b67c
Alex <moferalex@gmail.com>
2018-09-15 01:55:03 +0800

    [upload] 完成命名表预定义字段Get方法

--- opentype/src/main/java/am/util/opentype/tables/NameTable.java
+++ opentype/src/main/java/am/util/opentype/tables/NameTable.java
@@ -3,6 +3,7 @@ package am.util.opentype.tables;
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import am.util.opentype.FileOpenTypeReader;
 
@@ -10,6 +11,7 @@ import am.util.opentype.FileOpenTypeReader;
  * 命名表
  * Created by Alex on 2018/9/6.
  */
+@SuppressWarnings("unused")
 public class NameTable {
 
     private final int mFormat;// Format selector (0 or 1).
@@ -62,7 +64,6 @@ public class NameTable {
     private void initNames() throws IOException {
         if (mNameRecords == null || mNameRecords.isEmpty())
             return;
-        System.out.println("lallal----------------------------------------------------------------------------------------------------------------------------------------------");
         for (NameRecord record : mNameRecords) {
             final byte[] data = record.getData();
             if (data == null || data.length <= 0)
@@ -72,32 +73,33 @@ public class NameTable {
             final int languageID = record.getLanguageID();
             final String text;
             if (platformID == NameRecord.PLATFORM_WINDOWS) {
-                // TODO
-                if ((encodingID == NameRecord.ENCODING_WINDOWS_0 ||
-                        encodingID == NameRecord.ENCODING_WINDOWS_1) &&
-                        languageID == NameRecord.LANGUAGE_WINDOWS_0409) {
-                    text = new String(data, FileOpenTypeReader.CHARSET_UTF_16BE);
-                } else {
-                    if (data[0] == 0) {
+                if (encodingID == NameRecord.ENCODING_WINDOWS_0 ||
+                        encodingID == NameRecord.ENCODING_WINDOWS_1) {
+                    if (languageID == NameRecord.LANGUAGE_WINDOWS_0409)
+                        // 已验证
                         text = new String(data, FileOpenTypeReader.CHARSET_UTF_16BE);
-                    } else {
-                        text = new String(data, FileOpenTypeReader.CHARSET_ISO_8859_15);
-                    }
+                    else
+                        // 已验证
+                        text = new String(data, FileOpenTypeReader.CHARSET_UTF_16BE);
+                } else {
+                    // 未验证
+                    text = new String(data, FileOpenTypeReader.CHARSET_UTF_16BE);
                 }
 
             } else if (platformID == NameRecord.PLATFORM_MACINTOSH) {
-                // TODO
-                if (encodingID == NameRecord.ENCODING_MACINTOSH_0 &&
-                        languageID == NameRecord.LANGUAGE_MACINTOSH_0) {
-                    text = new String(data, FileOpenTypeReader.CHARSET_ISO_8859_15);
-                } else {
-                    if (data[0] == 0) {
-                        text = new String(data, FileOpenTypeReader.CHARSET_UTF_16BE);
-                    } else {
+                if (encodingID == NameRecord.ENCODING_MACINTOSH_0) {
+                    if (languageID == NameRecord.LANGUAGE_MACINTOSH_0)
+                        // 已验证
                         text = new String(data, FileOpenTypeReader.CHARSET_ISO_8859_15);
-                    }
+                    else
+                        // 未验证
+                        text = new String(data, FileOpenTypeReader.CHARSET_ISO_8859_15);
+                } else {
+                    // 未验证
+                    text = new String(data, FileOpenTypeReader.CHARSET_ISO_8859_15);
                 }
             } else {
+                // 未验证，应该存在问题
                 if (data[0] == 0) {
                     text = new String(data, FileOpenTypeReader.CHARSET_UTF_16BE);
                 } else {
@@ -200,5 +202,275 @@ public class NameTable {
         if (index < 0 || index >= mLangTags.size())
             return null;
         return mLangTags.get(index);
+    }
+
+    /**
+     * 获取格式
+     *
+     * @return 格式
+     */
+    public int getFormat() {
+        return mFormat;
+    }
+
+    /**
+     * 获取条目总数
+     *
+     * @return 总数
+     */
+    public int getCount() {
+        return mCount;
+    }
+
+    /**
+     * 获取字符串起始偏移（从表头第一个字节开始计算）
+     *
+     * @return 字符串起始偏移
+     */
+    public int getStringOffset() {
+        return mStringOffset;
+    }
+
+    /**
+     * 或去命名记录条目
+     *
+     * @return 命名记录条目
+     */
+    public List<NameRecord> getNameRecords() {
+        return mNameRecords;
+    }
+
+    /**
+     * 获取语言标签记录条目
+     *
+     * @return 语言标签记录条目
+     */
+    public List<LangTagRecord> getLangTagRecords() {
+        return mLangTagRecords;
+    }
+
+    /**
+     * 获取家族名
+     *
+     * @return 家族名
+     */
+    public Set<String> getFamilyNames() {
+        return mFamilyNames;
+    }
+
+    /**
+     * 获取印刷家族名
+     *
+     * @return 印刷家族名
+     */
+    public Set<String> getTypographicFamilyNames() {
+        return mTypographicFamilyNames;
+    }
+
+    /**
+     * 获取WWS家族名
+     *
+     * @return WWS家族名
+     */
+    public Set<String> getWWSFamilyNames() {
+        return mWWSFamilyNames;
+    }
+
+    /**
+     * 获取版权声明
+     *
+     * @return 版权申明
+     */
+    public String getCopyrightNotice() {
+        return mCopyrightNotice;
+    }
+
+    /**
+     * 获取样式名称（粗体、斜体、粗斜体）
+     *
+     * @return 样式名称
+     */
+    public String getSubfamilyName() {
+        return mSubfamilyName;
+    }
+
+    /**
+     * 获取唯一标志
+     *
+     * @return 唯一标志
+     */
+    public String getUID() {
+        return mUID;
+    }
+
+    /**
+     * 获取字体全称
+     *
+     * @return 字体全称
+     */
+    public String getFullName() {
+        return mFullName;
+    }
+
+    /**
+     * 获取版本信息
+     *
+     * @return 版本信息
+     */
+    public String getVersion() {
+        return mVersion;
+    }
+
+    /**
+     * 获取PostScript名
+     *
+     * @return PostScript名
+     */
+    public String getPostScriptName() {
+        return mPostScriptName;
+    }
+
+    /**
+     * 获取商标
+     *
+     * @return 商标
+     */
+    public String getTrademark() {
+        return mTrademark;
+    }
+
+    /**
+     * 获取字体供应商
+     *
+     * @return 字体供应商
+     */
+    public String getManufacturer() {
+        return mManufacturer;
+    }
+
+    /**
+     * 获取设计者
+     *
+     * @return 设计者
+     */
+    public String getDesigner() {
+        return mDesigner;
+    }
+
+    /**
+     * 获取字体描述
+     *
+     * @return 字体描述
+     */
+    public String getDescription() {
+        return mDescription;
+    }
+
+    /**
+     * 获取供应商链接
+     *
+     * @return 供应商链接
+     */
+    public String getURLVendor() {
+        return mURLVendor;
+    }
+
+    /**
+     * 获取设计者链接
+     *
+     * @return 设计者链接
+     */
+    public String getURLDesigner() {
+        return mURLDesigner;
+    }
+
+    /**
+     * 获取字体许可
+     *
+     * @return 字体许可
+     */
+    public String getLicense() {
+        return mLicense;
+    }
+
+    /**
+     * 获取字体许可链接
+     *
+     * @return 字体许可链接
+     */
+    public String getURLLicense() {
+        return mURLLicense;
+    }
+
+    /**
+     * 获取印刷样式名称
+     *
+     * @return 印刷样式名称
+     */
+    public String getTypographicSubfamilyName() {
+        return mTypographicSubfamilyName;
+    }
+
+    /**
+     * 获取Compatible Full名称（仅Mac平台需要）
+     *
+     * @return Compatible Full名称
+     */
+    public String getCompatible() {
+        return mCompatible;
+    }
+
+    /**
+     * 获取示例文本（用于展示字体效果）
+     *
+     * @return 示例文本
+     */
+    public String getSample() {
+        return mSample;
+    }
+
+    /**
+     * 获取PostScript CID
+     *
+     * @return PostScript CID
+     */
+    public String getPostScriptCID() {
+        return mPostScriptCID;
+    }
+
+    /**
+     * 获取WWS样式名称
+     *
+     * @return WWS样式名称
+     */
+    public String getWWSSubfamilyName() {
+        return mWWSSubfamilyName;
+    }
+
+    /**
+     * 获取浅色背景调色板
+     *
+     * @return 浅色背景调色板
+     */
+    public String getBackgroundPaletteLight() {
+        return mBackgroundPaletteLight;
+    }
+
+    /**
+     * 获取深色背景调色板
+     *
+     * @return 深色背景调色板
+     */
+    public String getBackgroundPaletteDark() {
+        return mBackgroundPaletteDark;
+    }
+
+    /**
+     * 获取变体PostScript名称前缀
+     *
+     * @return 变体PostScript名称前缀
+     */
+    public String getVariationsPostScriptNamePrefix() {
+        return mVariationsPostScriptNamePrefix;
     }
 }
