dbc4e33fa0f733faf06c5e0f7e5ecdadc7afc652
Alex <moferalex@gmail.com>
2018-10-23 00:23:31 +0800

    [update] 完成HHEA表对象equals、hashCOde及toString方法

--- opentype/src/main/java/am/util/opentype/tables/HorizontalHeaderTable.java
+++ opentype/src/main/java/am/util/opentype/tables/HorizontalHeaderTable.java
@@ -211,4 +211,55 @@ public class HorizontalHeaderTable extends BaseTable {
     public int getNumberOfHMetrics() {
         return mNumberOfHMetrics;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof HorizontalHeaderTable)) return false;
+        if (!super.equals(o)) return false;
+        HorizontalHeaderTable that = (HorizontalHeaderTable) o;
+        return mMajorVersion == that.mMajorVersion &&
+                mMinorVersion == that.mMinorVersion &&
+                mAscender == that.mAscender &&
+                mDescender == that.mDescender &&
+                mLineGap == that.mLineGap &&
+                mAdvanceWidthMax == that.mAdvanceWidthMax &&
+                mMinLeftSideBearing == that.mMinLeftSideBearing &&
+                mMinRightSideBearing == that.mMinRightSideBearing &&
+                mXMaxExtent == that.mXMaxExtent &&
+                mCaretSlopeRise == that.mCaretSlopeRise &&
+                mCaretSlopeRun == that.mCaretSlopeRun &&
+                mCaretOffset == that.mCaretOffset &&
+                mMetricDataFormat == that.mMetricDataFormat &&
+                mNumberOfHMetrics == that.mNumberOfHMetrics;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), mMajorVersion, mMinorVersion, mAscender, mDescender,
+                mLineGap, mAdvanceWidthMax, mMinLeftSideBearing, mMinRightSideBearing, mXMaxExtent,
+                mCaretSlopeRise, mCaretSlopeRun, mCaretOffset, mMetricDataFormat,
+                mNumberOfHMetrics);
+    }
+
+    @Override
+    public String toString() {
+        return "HorizontalHeaderTable{" +
+                "record=" + String.valueOf(getTableRecord()) +
+                ", majorVersion=" + mMajorVersion +
+                ", minorVersion=" + mMinorVersion +
+                ", ascender=" + mAscender +
+                ", descender=" + mDescender +
+                ", lineGap=" + mLineGap +
+                ", advanceWidthMax=" + mAdvanceWidthMax +
+                ", minLeftSideBearing=" + mMinLeftSideBearing +
+                ", minRightSideBearing=" + mMinRightSideBearing +
+                ", xMaxExtent=" + mXMaxExtent +
+                ", caretSlopeRise=" + mCaretSlopeRise +
+                ", caretSlopeRun=" + mCaretSlopeRun +
+                ", caretOffset=" + mCaretOffset +
+                ", metricDataFormat=" + mMetricDataFormat +
+                ", numberOfHMetrics=" + mNumberOfHMetrics +
+                '}';
+    }
 }
