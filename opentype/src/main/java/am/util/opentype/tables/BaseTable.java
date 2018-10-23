57a619303995f05ce7c5844d7c6612ebc6175d51
Alex <moferalex@gmail.com>
2018-10-24 00:32:25 +0800

    [update] 基础表中增加一次性获取hashCode及String，后续计算则使用缓存，因表不可修改，因此该值不可变。

--- opentype/src/main/java/am/util/opentype/tables/BaseTable.java
+++ opentype/src/main/java/am/util/opentype/tables/BaseTable.java
@@ -23,6 +23,8 @@ import am.util.opentype.TableRecord;
 public class BaseTable {
 
     private final TableRecord mRecord;
+    private Integer mHashCode = null;
+    private String mString = null;
 
     public BaseTable(TableRecord record) {
         mRecord = record;
@@ -38,21 +40,96 @@ public class BaseTable {
         return mRecord;
     }
 
+    @SuppressWarnings("ResultOfMethodCallIgnored")
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
+        if (mHashCode == null)
+            hashCode();
         BaseTable table = (BaseTable) o;
-        return Objects.equals(mRecord, table.mRecord);
+        if (table.mHashCode == null)
+            table.hashCode();
+        return Objects.equals(mHashCode, table.mHashCode);
     }
 
     @Override
     public int hashCode() {
+        if (mHashCode != null)
+            return mHashCode;
+        mHashCode = getHashCode();
+        return mHashCode;
+    }
+
+    /**
+     * Returns a hash code value for the object. This method is
+     * supported for the benefit of hash tables such as those provided by
+     * {@link java.util.HashMap}.
+     * <p>
+     * The general contract of {@code hashCode} is:
+     * <ul>
+     * <li>Whenever it is invoked on the same object more than once during
+     * an execution of a Java application, the {@code hashCode} method
+     * must consistently return the same integer, provided no information
+     * used in {@code equals} comparisons on the object is modified.
+     * This integer need not remain consistent from one execution of an
+     * application to another execution of the same application.
+     * <li>If two objects are equal according to the {@code equals(Object)}
+     * method, then calling the {@code hashCode} method on each of
+     * the two objects must produce the same integer result.
+     * <li>It is <em>not</em> required that if two objects are unequal
+     * according to the {@link java.lang.Object#equals(java.lang.Object)}
+     * method, then calling the {@code hashCode} method on each of the
+     * two objects must produce distinct integer results.  However, the
+     * programmer should be aware that producing distinct integer results
+     * for unequal objects may improve the performance of hash tables.
+     * </ul>
+     * <p>
+     * As much as is reasonably practical, the hashCode method defined by
+     * class {@code Object} does return distinct integers for distinct
+     * objects. (This is typically implemented by converting the internal
+     * address of the object into an integer, but this implementation
+     * technique is not required by the
+     * Java&trade; programming language.)
+     *
+     * @return a hash code value for this object.
+     * @see java.lang.Object#equals(java.lang.Object)
+     * @see java.lang.System#identityHashCode
+     */
+    protected int getHashCode() {
         return Objects.hash(mRecord);
     }
 
     @Override
     public String toString() {
+        if (mString != null)
+            return mString;
+        mString = getString();
+        return mString;
+    }
+
+    /**
+     * Returns a string representation of the object. In general, the
+     * {@code toString} method returns a string that
+     * "textually represents" this object. The result should
+     * be a concise but informative representation that is easy for a
+     * person to read.
+     * It is recommended that all subclasses override this method.
+     * <p>
+     * The {@code toString} method for class {@code Object}
+     * returns a string consisting of the name of the class of which the
+     * object is an instance, the at-sign character `{@code @}', and
+     * the unsigned hexadecimal representation of the hash code of the
+     * object. In other words, this method returns a string equal to the
+     * value of:
+     * <blockquote>
+     * <pre>
+     * getClass().getName() + '@' + Integer.toHexString(hashCode())
+     * </pre></blockquote>
+     *
+     * @return a string representation of the object.
+     */
+    protected String getString() {
         return "BaseTable{" +
                 "record=" + String.valueOf(mRecord) +
                 '}';
