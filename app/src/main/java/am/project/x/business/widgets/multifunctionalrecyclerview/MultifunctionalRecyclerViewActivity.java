e1252ddde2305942c79b41a179d089acd25a6b90
Alex <moferalex@gmail.com>
2018-11-13 00:41:54 +0800

    [update] 增加多功能回收布局基础页面

---
+++ app/src/main/java/am/project/x/business/widgets/multifunctionalrecyclerview/MultifunctionalRecyclerViewActivity.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2018 AlexMofer
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package am.project.x.business.widgets.multifunctionalrecyclerview;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import am.project.x.R;
+import am.project.x.base.BaseActivity;
+
+/**
+ * 多功能RecyclerView
+ */
+public class MultifunctionalRecyclerViewActivity extends BaseActivity {
+
+    public static void start(Context context) {
+        context.startActivity(new Intent(context, MultifunctionalRecyclerViewActivity.class));
+    }
+
+    @Override
+    protected int getContentViewLayout() {
+        return R.layout.activity_multifunctionalrecyclerview;
+    }
+
+    @Override
+    protected void initializeActivity(@Nullable Bundle savedInstanceState) {
+        setSupportActionBar(R.id.mrv_toolbar);
+    }
+}
