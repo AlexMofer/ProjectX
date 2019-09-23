5215c9793e3955c68665678528c8f012f0d56e18
Alex <moferalex@gmail.com>
2019-09-24 00:19:33 +0800

    [update] 修复销毁子项存储位置错误问题，并发布新版本

--- viewpager/src/main/java/am/util/viewpager/adapter/RecyclePagerAdapter.java
+++ viewpager/src/main/java/am/util/viewpager/adapter/RecyclePagerAdapter.java
@@ -22,11 +22,11 @@ import android.view.ConfigurationHelper;
 import android.view.View;
 import android.view.ViewGroup;
 
-import java.util.ArrayList;
-
 import androidx.annotation.NonNull;
 import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
+
+import java.util.ArrayList;
 
 /**
  * RecyclePagerAdapter
@@ -36,8 +36,8 @@ import androidx.viewpager.widget.ViewPager;
 public abstract class RecyclePagerAdapter<VH extends RecyclePagerAdapter.PagerViewHolder>
         extends PagerAdapter {
 
-    private ArrayList<VH> holderList = new ArrayList<>();
-    private SparseArray<ArrayList<VH>> holderSparse = new SparseArray<>();
+    private ArrayList<VH> mRunningItems = new ArrayList<>();
+    private SparseArray<ArrayList<VH>> mRecycledItems = new SparseArray<>();
 
     @Override
     public final int getCount() {
@@ -51,31 +51,33 @@ public abstract class RecyclePagerAdapter<VH extends RecyclePagerAdapter.PagerVi
     }
 
     @NonNull
+    @Override
     public final Object instantiateItem(@NonNull ViewGroup container, int position) {
         VH holder;
         int viewType = getItemViewType(position);
-        ArrayList<VH> recycleHolders = holderSparse.get(viewType);
+        ArrayList<VH> recycleHolders = mRecycledItems.get(viewType);
         if (recycleHolders != null && recycleHolders.size() > 0) {
             holder = recycleHolders.remove(0);
             holder.mPosition = POSITION_UNCHANGED;
+            holder.mViewType = viewType;
         } else {
             holder = createViewHolder(container, viewType);
         }
-        bindViewHolder(holder, position);
+        bindViewHolder(holder, position, viewType);
         container.addView(holder.itemView, 0);
         return holder;
     }
 
+    @Override
     public final void destroyItem(ViewGroup container, int position, @NonNull Object object) {
         //noinspection unchecked
         VH holder = (VH) object;
         container.removeView(holder.itemView);
         holder.isRecycled = true;
         holder.mPosition = POSITION_NONE;
-        int viewType = getItemViewType(position);
-        ArrayList<VH> recycleHolders = holderSparse.get(viewType, new ArrayList<VH>());
+        ArrayList<VH> recycleHolders = mRecycledItems.get(holder.mViewType, new ArrayList<VH>());
         recycleHolders.add(holder);
-        holderSparse.put(viewType, recycleHolders);
+        mRecycledItems.put(holder.mViewType, recycleHolders);
         onViewRecycled(holder);
     }
 
@@ -98,7 +100,7 @@ public abstract class RecyclePagerAdapter<VH extends RecyclePagerAdapter.PagerVi
         int position = POSITION_UNCHANGED;
         //noinspection unchecked
         VH holder = (VH) object;
-        if (holderList.contains(holder)) {
+        if (mRunningItems.contains(holder)) {
             position = holder.mPosition;
             position = position >= getItemCount() ? POSITION_NONE : position;
         }
@@ -114,12 +116,18 @@ public abstract class RecyclePagerAdapter<VH extends RecyclePagerAdapter.PagerVi
 
     public final VH createViewHolder(ViewGroup parent, int viewType) {
         final VH holder = onCreateViewHolder(parent, viewType);
-        holderList.add(holder);
+        mRunningItems.add(holder);
         return holder;
     }
 
+    @Deprecated
     public final void bindViewHolder(VH holder, int position) {
+        bindViewHolder(holder, position, getItemViewType(position));
+    }
+
+    public final void bindViewHolder(VH holder, int position, int viewType) {
         holder.mPosition = position;
+        holder.mViewType = viewType;
         holder.isRecycled = false;
         onBindViewHolder(holder, position);
     }
@@ -134,7 +142,7 @@ public abstract class RecyclePagerAdapter<VH extends RecyclePagerAdapter.PagerVi
     @Override
     public final void notifyDataSetChanged() {
         super.notifyDataSetChanged();
-        for (VH holder : holderList) {
+        for (VH holder : mRunningItems) {
             if (!holder.isRecycled && holder.mPosition < getItemCount()) {
                 onBindViewHolder(holder, holder.mPosition);
             }
@@ -142,7 +150,7 @@ public abstract class RecyclePagerAdapter<VH extends RecyclePagerAdapter.PagerVi
     }
 
     public final void notifyItemChanged(int position) {
-        for (VH holder : holderList) {
+        for (VH holder : mRunningItems) {
             if (!holder.isRecycled && holder.mPosition == position) {
                 onBindViewHolder(holder, holder.mPosition);
                 break;
@@ -151,9 +159,9 @@ public abstract class RecyclePagerAdapter<VH extends RecyclePagerAdapter.PagerVi
     }
 
     public void onConfigurationChanged(Configuration newConfig) {
-        final int count = holderSparse.size();
+        final int count = mRecycledItems.size();
         for (int i = 0; i < count; i++) {
-            final ArrayList<VH> holders = holderSparse.valueAt(i);
+            final ArrayList<VH> holders = mRecycledItems.valueAt(i);
             if (holders == null || holders.isEmpty())
                 continue;
             for (VH holder : holders) {
@@ -167,6 +175,7 @@ public abstract class RecyclePagerAdapter<VH extends RecyclePagerAdapter.PagerVi
     public static abstract class PagerViewHolder {
         public final View itemView;
         int mPosition = POSITION_UNCHANGED;
+        int mViewType = 0;
         boolean isRecycled = false;
 
         public PagerViewHolder(View itemView) {
