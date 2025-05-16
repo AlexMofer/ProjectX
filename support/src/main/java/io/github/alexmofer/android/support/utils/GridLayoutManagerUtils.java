cd079c74c66acd2859bac6735e84f14fd16a0832
Alex <alex@updf.cn>
2025-05-16 09:29:51 +0800

    [update] GridLayoutManager 工具

---
+++ support/src/main/java/io/github/alexmofer/android/support/utils/GridLayoutManagerUtils.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2025 AlexMofer
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.alexmofer.android.support.utils;
+
+import androidx.core.util.TypedValueCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+/**
+ * GridLayoutManager 工具
+ * Created by Alex on 2025/5/16.
+ */
+public class GridLayoutManagerUtils {
+
+    private GridLayoutManagerUtils() {
+        //no instance
+    }
+
+    /**
+     * 设置子项列数计算器
+     *
+     * @param view                  RecyclerView
+     * @param itemMinWidth          子项需要的最小宽度，DP为单位（以RecyclerView宽度进行均分）
+     * @param notifyItemDecorations 是否通知子项装饰刷新
+     */
+    public static void setSpanCountCalculator(RecyclerView view, int itemMinWidth, boolean notifyItemDecorations) {
+        view.addOnLayoutChangeListener(
+                (v, left, top, right, bottom, oldLeft, oldTop, oldRight, oldBottom) -> {
+                    final int width = v.getWidth();
+                    final float itemWidth =
+                            TypedValueCompat.dpToPx(itemMinWidth, v.getResources().getDisplayMetrics());
+                    final int count = Math.max(2, Math.round(width / itemWidth - 0.5f));
+                    final RecyclerView.LayoutManager manager = ((RecyclerView) v).getLayoutManager();
+                    if (manager instanceof GridLayoutManager) {
+                        v.post(() -> {
+                            ((GridLayoutManager) manager).setSpanCount(count);
+                            if (notifyItemDecorations) {
+                                ((RecyclerView) v).invalidateItemDecorations();
+                            }
+                        });
+                    }
+                });
+    }
+}
