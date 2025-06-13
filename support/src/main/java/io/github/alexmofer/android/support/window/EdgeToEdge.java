f6ec8b567f4ddbc5dd9f8b697ef58a03b4ebe99b
Alex <alex@updf.cn>
2025-06-13 10:54:49 +0800

    [update] 增加边到边工具

--- support/src/main/java/io/github/alexmofer/android/support/window/EdgeToEdge.java
+++ support/src/main/java/io/github/alexmofer/android/support/window/EdgeToEdge.java
@@ -1,30 +1,113 @@
-package io.github.alexmofer.projectx.utils;
+/*
+ * Copyright (C) 2025 AlexMofer
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.alexmofer.android.support.window;
 
+import android.app.Activity;
 import android.app.UiModeManager;
+import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.graphics.Color;
+import android.os.Build;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
+import androidx.annotation.ColorInt;
 import androidx.annotation.RequiresApi;
 import androidx.core.view.WindowCompat;
 import androidx.core.view.WindowInsetsControllerCompat;
-
-import java.util.function.Function;
-
-import io.github.alexmofer.projectx.utils.EdgeToEdge.SystemBarStyle;
 
 /**
  * 边到边
  * Created by Alex on 2025/6/12.
  */
 public class EdgeToEdge {
+    // The light scrim color used in the platform API 29+
+    // https://cs.android.com/android/platform/superproject/+/master:frameworks/base/core/java/com/android/internal/policy/DecorView.java;drc=6ef0f022c333385dba2c294e35b8de544455bf19;l=142
+    private static final int DefaultLightScrim = Color.argb(0xe6, 0xFF, 0xFF, 0xFF);
+
+    // The dark scrim color used in the platform.
+    // https://cs.android.com/android/platform/superproject/+/master:frameworks/base/core/res/res/color/system_bar_background_semi_transparent.xml
+    // https://cs.android.com/android/platform/superproject/+/master:frameworks/base/core/res/remote_color_resources_res/values/colors.xml;l=67
+    private static final int DefaultDarkScrim = Color.argb(0x80, 0x1b, 0x1b, 0x1b);
+    private static EdgeToEdgeImpl Impl = null;
 
     private EdgeToEdge() {
         //no instance
     }
 
+    public static void enable(SystemBarStyle statusBarStyle,
+                              SystemBarStyle navigationBarStyle,
+                              Window window,
+                              View view,
+                              boolean statusBarIsDark,
+                              boolean navigationBarIsDark) {
+        if (Impl == null) {
+            if (Build.VERSION.SDK_INT >= 30) {
+                Impl = new EdgeToEdgeApi30();
+            } else if (Build.VERSION.SDK_INT >= 29) {
+                Impl = new EdgeToEdgeApi29();
+            } else if (Build.VERSION.SDK_INT >= 28) {
+                Impl = new EdgeToEdgeApi28();
+            } else if (Build.VERSION.SDK_INT >= 26) {
+                Impl = new EdgeToEdgeApi26();
+            } else if (Build.VERSION.SDK_INT >= 23) {
+                Impl = new EdgeToEdgeApi23();
+            } else if (Build.VERSION.SDK_INT >= 21) {
+                Impl = new EdgeToEdgeApi21();
+            } else {
+                Impl = new EdgeToEdgeBase();
+            }
+        }
+        Impl.setUp(statusBarStyle, navigationBarStyle, window, view, statusBarIsDark, navigationBarIsDark);
+        Impl.adjustLayoutInDisplayCutoutMode(window);
+    }
+
+    public static void enable(Activity activity, SystemBarStyle statusBarStyle,
+                              SystemBarStyle navigationBarStyle) {
+        final Window window = activity.getWindow();
+        final View view = window.getDecorView();
+        final boolean statusBarIsDark = Boolean.TRUE.equals(
+                statusBarStyle.detectDarkMode.apply(view.getResources()));
+        final boolean navigationBarIsDark = Boolean.TRUE.equals(
+                navigationBarStyle.detectDarkMode.apply(view.getResources()));
+        enable(statusBarStyle, navigationBarStyle, window, view, statusBarIsDark,
+                navigationBarIsDark);
+    }
+
+    public static void enable(Activity activity) {
+        enable(activity, SystemBarStyle.auto(Color.TRANSPARENT, Color.TRANSPARENT),
+                SystemBarStyle.auto(DefaultLightScrim, DefaultDarkScrim));
+    }
+
+    public static void enable(Window window, SystemBarStyle statusBarStyle,
+                              SystemBarStyle navigationBarStyle) {
+        final View view = window.getDecorView();
+        final boolean statusBarIsDark = Boolean.TRUE.equals(
+                statusBarStyle.detectDarkMode.apply(view.getResources()));
+        final boolean navigationBarIsDark = Boolean.TRUE.equals(
+                navigationBarStyle.detectDarkMode.apply(view.getResources()));
+        enable(statusBarStyle, navigationBarStyle, window, view, statusBarIsDark,
+                navigationBarIsDark);
+    }
+
+    public static void enable(Window window) {
+        enable(window, SystemBarStyle.auto(Color.TRANSPARENT, Color.TRANSPARENT),
+                SystemBarStyle.auto(DefaultLightScrim, DefaultDarkScrim));
+    }
 
     private interface EdgeToEdgeImpl {
 
@@ -132,59 +215,46 @@ public class EdgeToEdge {
         }
     }
 
+    // API24 以后可使用 Function<Resources, Boolean> 替代
+    public interface DarkModeDetector {
+        boolean apply(Resources t);
+    }
+
     public static class SystemBarStyle {
         protected final int lightScrim;
         protected final int darkScrim;
         protected final int nightMode;
-        protected final Function<Resources, Boolean> detectDarkMode;
+        protected final DarkModeDetector detectDarkMode;
 
         public SystemBarStyle(int lightScrim, int darkScrim, int nightMode,
-                              Function<Resources, Boolean> detectDarkMode) {
+                              DarkModeDetector detectDarkMode) {
             this.lightScrim = lightScrim;
             this.darkScrim = darkScrim;
             this.nightMode = nightMode;
             this.detectDarkMode = detectDarkMode;
         }
 
-        fun auto(
-                @ColorInt lightScrim: Int,
-                @ColorInt darkScrim: Int,
-                detectDarkMode: (Resources) -> Boolean = { resources ->
-                (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) ==
-            Configuration.UI_MODE_NIGHT_YES
-        }
-        ): SystemBarStyle {
-            return SystemBarStyle(
-                    lightScrim = lightScrim,
-                    darkScrim = darkScrim,
-                    nightMode = UiModeManager.MODE_NIGHT_AUTO,
-                    detectDarkMode = detectDarkMode
-            )
+        public static SystemBarStyle auto(@ColorInt int lightScrim, @ColorInt int darkScrim) {
+            return new SystemBarStyle(lightScrim, darkScrim, UiModeManager.MODE_NIGHT_AUTO,
+                    resources -> (resources.getConfiguration().uiMode
+                            & Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES);
         }
 
-        fun dark(@ColorInt scrim: Int): SystemBarStyle {
-            return SystemBarStyle(
-                    lightScrim = scrim,
-                    darkScrim = scrim,
-                    nightMode = UiModeManager.MODE_NIGHT_YES,
-                    detectDarkMode = { _ -> true }
-            )
+        public static SystemBarStyle dark(@ColorInt int scrim) {
+            return new SystemBarStyle(scrim, scrim, UiModeManager.MODE_NIGHT_YES,
+                    resources -> true);
         }
 
-        fun light(@ColorInt scrim: Int, @ColorInt darkScrim: Int): SystemBarStyle {
-            return SystemBarStyle(
-                    lightScrim = scrim,
-                    darkScrim = darkScrim,
-                    nightMode = UiModeManager.MODE_NIGHT_NO,
-                    detectDarkMode = { _ -> false }
-            )
+        public static SystemBarStyle light(@ColorInt int scrim, @ColorInt int darkScrim) {
+            return new SystemBarStyle(scrim, darkScrim, UiModeManager.MODE_NIGHT_NO,
+                    resources -> false);
         }
 
-        public int getScrim(boolean isDark)  {
+        public int getScrim(boolean isDark) {
             return isDark ? darkScrim : lightScrim;
         }
 
-        public int getScrimWithEnforcedContrast(boolean isDark)  {
+        public int getScrimWithEnforcedContrast(boolean isDark) {
             if (nightMode == UiModeManager.MODE_NIGHT_AUTO) {
                 return Color.TRANSPARENT;
             }
