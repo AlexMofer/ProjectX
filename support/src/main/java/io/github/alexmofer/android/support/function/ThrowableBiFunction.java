3ac5e5e3a66d1048f221b40e67b06ec7bfa29c67
Alex <alex@updf.cn>
2025-08-16 17:28:14 +0800

    [update] 增加接口

---
+++ support/src/main/java/io/github/alexmofer/android/support/function/ThrowableBiFunction.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2025 AlexMofer
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.alexmofer.android.support.function;
+
+import java.util.function.Function;
+
+/**
+ * Represents a function that accepts two arguments and produces a result.
+ * This is the two-arity specialization of {@link Function}.
+ *
+ * <p>This is a <a href="package-summary.html">functional interface</a>
+ * whose functional method is {@link #apply(Object, Object)}.
+ *
+ * @param <T> the type of the first argument to the function
+ * @param <U> the type of the second argument to the function
+ * @param <R> the type of the result of the function
+ * @see Function
+ * @since 1.8
+ */
+@FunctionalInterface
+public interface ThrowableBiFunction<T, U, R> {
+
+    /**
+     * Applies this function to the given arguments.
+     *
+     * @param t the first function argument
+     * @param u the second function argument
+     * @return the function result
+     */
+    R apply(T t, U u) throws Exception;
+}
