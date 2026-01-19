37e134e632bb2a46afc4c46f485db47332fd15d5
Alex <alex@updf.cn>
2026-01-19 18:51:15 +0800

    [update] 增加颜色动画接口。

--- support/src/main/java/io/github/alexmofer/android/support/utils/ColorUtils.java
+++ support/src/main/java/io/github/alexmofer/android/support/utils/ColorUtils.java
@@ -144,4 +144,55 @@ public class ColorUtils {
                     && Math.abs(Color.blue(color1) - Color.blue(color2)) <= 3;
         }
     }
+
+    /**
+     * This function returns the calculated in-between value for a color
+     * given integers that represent the start and end values in the four
+     * bytes of the 32-bit int. Each channel is separately linearly interpolated
+     * and the resulting calculated values are recombined into the return value.
+     *
+     * @param fraction   The fraction from the starting to the ending values
+     * @param startColor A 32-bit int value representing colors in the
+     *                   separate bytes of the parameter
+     * @param endColor   A 32-bit int value representing colors in the
+     *                   separate bytes of the parameter
+     * @return A value that is calculated to be the linearly interpolated
+     * result, derived by separating the start and end values into separate
+     * color channels and interpolating each one separately, recombining the
+     * resulting values in the same way.
+     */
+    public static int animate(float fraction, int startColor, int endColor) {
+        float startA = ((startColor >> 24) & 0xff) / 255.0f;
+        float startR = ((startColor >> 16) & 0xff) / 255.0f;
+        float startG = ((startColor >> 8) & 0xff) / 255.0f;
+        float startB = (startColor & 0xff) / 255.0f;
+
+        float endA = ((endColor >> 24) & 0xff) / 255.0f;
+        float endR = ((endColor >> 16) & 0xff) / 255.0f;
+        float endG = ((endColor >> 8) & 0xff) / 255.0f;
+        float endB = (endColor & 0xff) / 255.0f;
+
+        // convert from sRGB to linear
+        startR = (float) Math.pow(startR, 2.2);
+        startG = (float) Math.pow(startG, 2.2);
+        startB = (float) Math.pow(startB, 2.2);
+
+        endR = (float) Math.pow(endR, 2.2);
+        endG = (float) Math.pow(endG, 2.2);
+        endB = (float) Math.pow(endB, 2.2);
+
+        // compute the interpolated color in linear space
+        float a = startA + fraction * (endA - startA);
+        float r = startR + fraction * (endR - startR);
+        float g = startG + fraction * (endG - startG);
+        float b = startB + fraction * (endB - startB);
+
+        // convert back to sRGB in the [0..255] range
+        a = a * 255.0f;
+        r = (float) Math.pow(r, 1.0 / 2.2) * 255.0f;
+        g = (float) Math.pow(g, 1.0 / 2.2) * 255.0f;
+        b = (float) Math.pow(b, 1.0 / 2.2) * 255.0f;
+
+        return Math.round(a) << 24 | Math.round(r) << 16 | Math.round(g) << 8 | Math.round(b);
+    }
 }
